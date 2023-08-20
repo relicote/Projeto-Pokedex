1bad26553719c668b8569b837055e81879c3d094
Gabriel Reliquias <125402939+relicote@users.noreply.github.com>
2023-08-20 12:03:58 -0300

    att

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,10 @@
-console.log('opa')
+const offset = 0
+const limit = 10
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+// Fetch retorna uma Promise - Uma promessa de um resultado
+// Assincrono (um processo executado que a resposta não é imediata)
+ 
+fetch(url).then(function(response){
+    console.log(response);
+})
