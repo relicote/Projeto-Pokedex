156a033ab61790594f7bbed35215cf9754aa93db
Gabriel Reliquias <125402939+relicote@users.noreply.github.com>
2023-08-20 22:03:04 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,18 +4,20 @@ const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
 // Fetch retorna uma Promise - Uma promessa de um resultado
 // Assincrono (um processo executado que a resposta não é imediata)
- 
-fetch(url).then(function(response){
-    response.json().then(function(responseBody){
-        console.log(responseBody)
+
+fetch(url)
+    .then(function (response) {
+        return response.json()
     })
-})
 
+    .then(function (jsonBody) {
+        console.log(jsonBody)
+    })
 
-.catch(function(error){
-    console.log(error)
-})
+    .catch(function (error) {
+        console.log(error)
+    })
 
-.finally(function(){
-    console.log('Requisicão conluída!')
-})
+    .finally(function () {
+        console.log('Requisicão conluída!')
+    })
